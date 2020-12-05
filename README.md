55ffc3e2018112f149ef6f9ea2bb827c95e777a6
earl <placidej@myumanitoba.ca>
2020-12-05 17:14:35 -0600

    Finish up data cleaning stuff

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # VfDM
 Data mining project
 
-# How to install dependencies
+# Preprocess.py
+## How to install dependencies
 
 `pip install -r requirements.txt`
+## How to run
+
+`py preprocess.py -fp [FILEPATH] -ds [DATE_START] -de [DATE_END]`
+
+- example: `py preprocess.py -fp C-Questions.csv -ds 2008-01-01 -de 2018-01-01`
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,58 @@
 #!/usr/bin/env python
+
 import argparse
 import re
+import pandas as pd
 from datetime import datetime
-from bs4 import BeautifulSoup
+def main():
+    args = init_parser().parse_args()
+
+    csv_file = args.filePath
+    out_file = args.outputFile
+    date_start = args.dateStart
+    date_end = args.dateEnd
+    
+    df = pd.read_csv(csv_file, parse_dates=['CreationDate'])
+
+    filtered = filter_df(df, date_start, date_end)
+    cleaned = clean_df(filtered)
+    df_to_csv(cleaned, out_file)
+
+
+def filter_df(df, date_start = None, date_end = None):
+    filtered = df
+
+    # filter by date range if both dates are provided
+    if date_start and date_end:
+        filtered = df.loc[(df['CreationDate'] > pd.to_datetime(date_start)) & (df['CreationDate'] < pd.to_datetime(date_end))]
+
+    # filter title and body
+    filtered = filtered[['Id', 'Title', 'Body']]
+    
+    # make body and title columns into their own rows
+    filtered = filtered.melt(id_vars = ['Id'], var_name = "Title", value_name = "Text")
+    
+    # filter again
+    filtered = filtered[['Text']]
+
+    print(filtered)
+
+    return filtered
+
+
+def clean_df(df):
+
+    # remove code blocks
+    regex_code = re.compile('<code>.*?</code>', flags=re.DOTALL)
+    cleaned = df.replace(regex_code, '', regex=True)
+
+    # remove html blocks
+    cleaned = cleaned.replace('<[^>]+>', '', regex=True)
+    return cleaned
+
+def df_to_csv(df, output_path):
+    df.to_csv(output_path, index=None)
+
 
 def init_parser():
     parser = argparse.ArgumentParser(
@@ -14,69 +64,23 @@ def init_parser():
                         '--filePath',
                         required=True,
                         help="Path to file")
-
                         
     parser.add_argument('-o',
                         '--outputFile',
-                        default="output.csv",
+                        default="./output/output.csv",
                         help="Specific output name")
     
     parser.add_argument('-ds',
                         '--dateStart',
+                        type=lambda d: datetime.strptime(d, '%Y-%m-%d').date(),
                         help="Date start (DD-MM-YYYY")
 
     parser.add_argument('-de',
                         '--dateEnd',
+                        type=lambda d: datetime.strptime(d, '%Y-%m-%d').date(),
                         help="Date end (DD-MM-YYYY)")
 
     return parser
 
-def access_file(file_path):
-    text = ''
-
-    with open(file_path, 'rb') as file:
-        text = file.read().decode()
-        file.close()
-    
-    return text
-
-
-def write_file(text, file_path):
-
-    new_file = open(file_path, "w", encoding='utf-8')
-    new_file.write(text)
-    new_file.close()
-
-
-def clean_text(text):
-    soup = BeautifulSoup(text)
-    clean = soup.get_text()
-    clean = re.sub('<code>.*?</code>','', text, flags=re.DOTALL)
-    
-    return clean
-
-
 if __name__ == '__main__':
-    args = init_parser().parse_args()
-
-    csv_file = args.filePath
-    out_file = args.outputFile
-    date_start = args.dateStart
-    date_end = args.dateEnd
-
-    print(csv_file)
-    print(out_file)
-    print(date_start)
-    print(date_end)
-
-    file = access_file(csv_file)
-
-    cleaned = clean_text(file)
-
-    write_file(cleaned, out_file)
-
-
-
-
-
-
+    main()
