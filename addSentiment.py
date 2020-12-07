f2c560df029555fd8a4e1f471fda2b7c10fac9cf
chad <chadhillary2013@gmail.com>
2020-12-06 18:08:00 -0600

    Fixed addSentiment.py

--- addSentiment.py
+++ addSentiment.py
@@ -9,16 +9,23 @@ sentiFile = sys.argv[1]
 postFile = sys.argv[2]
 
 try:
-    sIn = pd.read_csv(sentiFile, sep=",", header=0)
+    sIn = pd.read_csv(sentiFile, sep=",", decimal="t", header=0, dtype='str')
     posts = pd.read_csv(postFile)
 except:
     exit
 
 outPath = str(pathlib.Path().absolute()) + "\Sentiment_" + postFile
-
-print(outPath)
+#print(outPath)
 sIn.columns = ["tID", "Sentiment"]
-sIn = sIn.sort_values('tID', ascending=True)
+
+sIn['index'] = sIn['tID'].str.slice(1)
+sIn['index'] = sIn['index'].astype(int)
+sIn.set_index('index', inplace=True, verify_integrity=True) # return false if duplacite tID's in ssentiFile
+
+# Sorting below should be unneccesary - can be useful for cheking debug output though
+# sIn.sort_values('index', inplace=True, ascending=True)
+# print(sIn)
+
 posts = posts.join(sIn["Sentiment"])
 
 # DEBUG (Can Remove): Print first 15 rows to preview data
